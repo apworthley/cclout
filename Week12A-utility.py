cf95c2ea390887a7374eba66e9e00efa159c587c
Drew Worthley <apworthley@mymail.mines.edu>
2019-11-20 20:05:49 -0700

    Added FindWord Count

--- Week12A-utility.py
+++ Week12A-utility.py
@@ -9,15 +9,23 @@ def PrintOutput (output):
 def LoadFile (file):
     f = open (file , "r")
     old = []
-   
+    new =[]
     for line in f:
         old.append(line)
-    return old
+    f.close()
+    for i in old:
+        if "\n" in i:
+            length= len(i)-1
+            new.append(i[:length])
+        else:
+            new.append(i)
+    return new
 def UpdateString (word, letter, number):
     newstring=word[:number]+letter+ word[number+1:]
     return newstring
-def main():
-    PrintOutput("lol")
-    print (LoadFile("a.txt"))
-    print (UpdateString("Andrew", "bitch", 2))
-main()
+def FindWordCount(a, findword):
+    count=0
+    for i in a:
+        count+= i.count(findword)
+    return count
+
